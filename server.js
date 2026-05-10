d11772f60671c7044f67d9e878fc3517ec69020b
sufyan <safianumoney100@gmail.com>
2026-05-10 18:48:45 +0100

    code fix

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-const io = new Server(server);
 
 const io = new Server(server, {
   cors: {
